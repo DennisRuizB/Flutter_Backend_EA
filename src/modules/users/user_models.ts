1a26eb32ad3dd4b6db44c9ee113f1d30b488567b
DennisRuizB <127039219+DennisRuizB@users.noreply.github.com>
2025-05-03 09:30:39 +0200

    Forked Repo

---
+++ src/modules/users/user_models.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name :{
+        type: String,
+        required : true
+    },
+    age: {
+        type: Number,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password: {
+        type : String,
+        required : true
+    }
+});
+
+export interface IUser{
+    name : string;
+    age : number;
+    email : string;
+    password: string;
+
+}
+
+const User = mongoose.model('User', userSchema);
+export default User;
